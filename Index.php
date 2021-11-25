7ab95c6c412634f5951740c1a35bcc6c91817b86
Javier Borbon <santiagoborbon@gmail.com>
2021-11-25 20:45:57 +0100

    Footer acabado sin links

---
+++ Index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rituals | Home</title>
+    <link href="https://www.rituals.com/on/demandware.static/Sites-IBE-Site/-/default/dw48c20db6/images/favicon.ico" rel="shortcut icon">
+    <meta name="description" content="Descubre Rituals. Cosmética Cuerpo y Hogar diseñados para ayudarte a relajarte en casa.">
+    <link href="estilos.css" rel="stylesheet">
+</head>
+<body>
+
+
+  <?php
+        include "footer.php";
+    ?>
+</body>
+</html>
